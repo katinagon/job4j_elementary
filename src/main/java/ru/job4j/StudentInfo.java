7f7dfb94d0d001acbea90d18390fe95f868c6dd2
Ekaterina Goncharova <katinagon@yandex.ru>
2023-01-27 17:05:19 +0800

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Ekaterina Goncharova");
+        System.out.println("11.04.1996");
+    }
+}
