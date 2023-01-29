9f826af0d6d757b48e87a61d7d0684f3c27f4d2d
Ekaterina Goncharova <katinagon@yandex.ru>
2023-01-29 11:40:45 +0800

    6. Идеальный вес [#41584]

---
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -0,0 +1,20 @@
+package ru.job4j.calculator;
+
+public class Fit {
+    public static double manWeight(short height) {
+        return (height - 100) * 1.15;
+    }
+
+    public static double womanWeight(short height) {
+        return (height - 110) * 1.15;
+    }
+
+    public static void main(String[] args) {
+        short heightMan = 187;
+        short heightWoman = 162;
+        double man = Fit.manWeight(heightMan);
+        double woman = Fit.womanWeight(heightWoman);
+        System.out.println("Man 187 is " + man);
+        System.out.println("Woman 162 is " + woman);
+    }
+}
